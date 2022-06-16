deea658efdae1eb6343e346debcbc70c6304ea0c
Wilson Soares <wilson.sfj@outlook.com>
2022-06-16 11:50:20 -0300

    configuring postgres database

--- finance/settings.py
+++ finance/settings.py
@@ -52,10 +52,14 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'finance.wsgi.application'
 
-DATABASES = {
+DATABASES = { 
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'admin',
+        'PASSWORD': 'root',
+        'HOST': 'postgres',
+        'PORT': '5432',
     }
 }
 
