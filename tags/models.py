36ce966505358a9df3fa4330096a581c969e6df8
Wilson Soares <wilson.sfj@outlook.com>
2022-06-26 13:28:02 -0300

    documenting tags and user apis

--- tags/models.py
+++ tags/models.py
@@ -3,6 +3,10 @@ from utils.base_model import BaseModel
 
 
 class TagModel(BaseModel):
+    """
+    Stores a single tag entry, related to :model:`user.UserModel`.
+
+    """
     description = models.CharField(max_length=100)
     user = models.ForeignKey('user.UserModel', related_name='user_tags', on_delete=models.CASCADE)
 
@@ -10,3 +14,10 @@ class TagModel(BaseModel):
         db_table = 'tag'
         verbose_name = 'tag'
         verbose_name_plural = 'tags'
+
+    def __str__(self) -> str:
+        """
+        Show the name of tag in the class instance
+
+        """
+        return self.description
